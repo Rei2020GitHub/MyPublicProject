cc85bfb82e3600f58c922542f5de22153a69f4db
l00590538 <h2m0r0r0@gmail.com>
2021-09-22 20:24:41 +0900

    Update check update demo

--- CheckUpdtae/app/src/main/java/com/sample/hmssample/checkupdtae/MainActivity.kt
+++ CheckUpdtae/app/src/main/java/com/sample/hmssample/checkupdtae/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.sample.hmssample.checkupdtae
 
 import android.content.Intent
+import android.content.pm.ApplicationInfo
 import android.content.pm.PackageManager
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
@@ -15,6 +16,9 @@ class MainActivity : AppCompatActivity() {
     companion object {
         private const val PLAY_STORE_LINK = "https://play.google.com/store/apps/details?id=com.huaweioverseas.smarthome"
         private const val APPGALLERY_LINK = "market://com.huawei.appmarket.applink?appId=C10406921"
+
+        private const val PLAY_STORE_PACKAGENAME = "com.android.vending"
+        private const val APPGALLERY_PACKAGENAME = "com.huawei.appmarket"
     }
 
     private lateinit var binding: ActivityMainBinding
@@ -40,23 +44,44 @@ class MainActivity : AppCompatActivity() {
         // ↓
         // 最新バージョンではない場合
         // ↓
-        updateApp()
+        updateAppByCheckingPackage()
     }
 
-    private fun updateApp() {
+    private fun updateAppByCheckingService() {
         // GMSの存在確認
         val googleResult = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(this)
 
         // HMSの存在確認
         val huaweiResult = HuaweiApiAvailability.getInstance().isHuaweiMobileServicesAvailable(this)
 
-        // 先にPlay Storeが使えるかどうか調べる
+        // 先にGMSが使えるかどうか調べる
+        // GMSが使える場合、Play Storeが存在するとみなす
         if (com.google.android.gms.common.ConnectionResult.SUCCESS == googleResult) {
             openLink(PLAY_STORE_LINK)
         }
-        // Play Storeが使えない場合、AppGalleryが使えるかどうか調べる
+        // GMSが使えない場合、HMSが使えるかどうか調べる
+        // HMSが使える場合、AppGalleryが存在するとみなす
         else if (com.huawei.hms.api.ConnectionResult.SUCCESS == huaweiResult) {
             openLink(APPGALLERY_LINK)
+        }
+    }
+
+    private fun updateAppByCheckingPackage() {
+        // 先にPlay Storeが使えるかどうか調べる
+        if (getApplicationInfo(PLAY_STORE_PACKAGENAME) != null) {
+            openLink(PLAY_STORE_LINK)
+        }
+        // Play Storeが使えない場合、AppGalleryが使えるかどうか調べる
+        else if (getApplicationInfo(APPGALLERY_PACKAGENAME) != null) {
+            openLink(APPGALLERY_LINK)
+        }
+    }
+
+    private fun getApplicationInfo(packageName: String): ApplicationInfo? {
+        return try {
+            packageManager.getApplicationInfo(packageName, 0)
+        } catch (exception: PackageManager.NameNotFoundException) {
+            null
         }
     }
 
